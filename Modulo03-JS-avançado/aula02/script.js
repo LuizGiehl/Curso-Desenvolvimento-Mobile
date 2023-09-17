69804d6bdf348c52c9c5da0c328e5124867d9a01
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2023-09-17 14:28:22 -0300

    Update script.js

--- Modulo03-JS-avançado/aula02/script.js
+++ Modulo03-JS-avançado/aula02/script.js
@@ -11,7 +11,7 @@ function calcular(event) {
     altura = document.getElementById('altura').value
     imc = peso / (altura**2)
     
-    resultado.innerHTML = `Seu IMC é de: ${imc}`
+    resultado.innerHTML = `Seu IMC é de: ${imc.toFixed(2)}`
 
     if(imc <= 18.5){
         resultado.innerHTML += `<br> Você está abaixo do pesso`
@@ -22,7 +22,7 @@ function calcular(event) {
     else if (imc >= 25 && imc <= 29.9){
         resultado.innerHTML += `<br> Você está com sobrepeso`
     }
-    else if (imc >= 30 && imc <= 39.9){
+    else if (imc >= 30 && imc <= 34.9){
         resultado.innerHTML += `<br> Você está com obesidade de grau I`
     }
     else if (imc >= 35 && imc <= 39.9) {
