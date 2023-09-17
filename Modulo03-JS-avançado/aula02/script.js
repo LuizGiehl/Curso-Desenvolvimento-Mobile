0bb0c632331bcf42f6afa46a40cd4cd68aca57d4
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2023-09-17 11:59:22 -0300

    aula 02
    Calculadora de IMC

---
+++ Modulo03-JS-avançado/aula02/script.js
@@ -0,0 +1,38 @@
+var peso;
+var altura;
+var imc;
+
+
+function calcular(event) {
+    event.preventDefault();
+    var resultado = document.getElementById('resultado')
+
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value
+    imc = peso / (altura**2)
+    
+    resultado.innerHTML = `Seu IMC é de: ${imc}`
+
+    if(imc <= 18.5){
+        resultado.innerHTML += `<br> Você está abaixo do pesso`
+    }
+    else if(imc >= 18.6 && imc <= 24.9){
+        resultado.innerHTML += `<br> Você está no peso ideal`
+    }
+    else if (imc >= 25 && imc <= 29.9){
+        resultado.innerHTML += `<br> Você está com sobrepeso`
+    }
+    else if (imc >= 30 && imc <= 39.9){
+        resultado.innerHTML += `<br> Você está com obesidade de grau I`
+    }
+    else if (imc >= 35 && imc <= 39.9) {
+        resultado.innerHTML += `<br> Você está com obesidade grau II`
+    }
+    else if(imc >= 40) {
+        resultado.innerHTML += `<br> Procure ajuda! Você está com obesidade mórbida`
+    }
+
+    console.log(imc)
+    console.log(peso)
+    console.log(altura)
+}
