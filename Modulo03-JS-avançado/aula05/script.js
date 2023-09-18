4e70ac3cb20493523f122054ec70903f2f6a0974
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2023-09-18 20:15:23 -0300

    aula 4 e 5

---
+++ Modulo03-JS-avançado/aula05/script.js
@@ -0,0 +1,25 @@
+var pessoa1 = {
+    nome: 'luiz',
+    idade: 16,
+    profissao: 'estudante'
+}
+
+var pessoa2 = {
+    nome: 'alberto',
+    idade: 46,
+    profissao: 'programador java senior'
+}
+
+var pessoa3 = {
+    nome: 'cleidosvalto',
+    idade: 43,
+    profissao: 'programador fullstack senior'
+}
+
+let pessoas = [
+    {pessoa1},
+    {pessoa2},
+    {pessoa3}
+]
+
+console.log(pessoas)
