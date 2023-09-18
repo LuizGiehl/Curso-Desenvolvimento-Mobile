fa4be2d7274edae93955f71dca63e9c4ce991d21
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2023-09-18 19:30:37 -0300

    aula03

---
+++ Modulo03-JS-avançado/aula03/script.js
@@ -0,0 +1,11 @@
+function acao(){
+    document.write('Execultando... <br>')
+}
+
+
+/*var timer = setInterval(() => {
+    document.write('Execultando!!! <br>')
+}, 1000)
+*/
+
+setTimeout(acao, 3000)
