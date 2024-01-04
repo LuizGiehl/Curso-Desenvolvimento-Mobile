48e577581cc133f50a0eebfe6797135d7c365ed9
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2024-01-04 15:07:49 -0300

    aula 10

--- Modulo06-basico-React/aula10/index.js
+++ Modulo06-basico-React/aula10/index.js
@@ -1,30 +1,17 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput} from 'react-native';
 
 
 function App() {
   const [nome, setNome] = useState('');
 
-  function pegaNome(texto) {
-    
-
-    if(texto.length > 0) {
-      setNome('Bem vindo ' + texto);
-    } else {
-      setNome('')
-    }
-  }
-
   return(
     <View style={styles.container}>
 
       <TextInput
-        style={styles.input}
+        styles={styles.input}
         placeholder='Digite seu nome'
-        onChangeText={ (text) => pegaNome(text) }
       />
-
-      <Text style={styles.texto}>{nome}</Text>
 
     </View>
   );
@@ -33,21 +20,15 @@ function App() {
 const styles = StyleSheet.create({
   container:{
     flex:1,
+    backgroundColor: 'gray'
   },
 
   input:{
     height: 45,
     borderWidth:2,
     margin: 10,
-    marginTop: 45,
     padding:10,
     fontSize: 20
-  },
-
-  texto:{
-    textAlign: 'center',
-    fontSize: 25,
-
   }
 });
 
