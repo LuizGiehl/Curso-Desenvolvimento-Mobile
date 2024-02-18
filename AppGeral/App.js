33a73332a85c034740a0911750e68646b66499d4
Luiz Otávio Silva Giehl <139135154+LuizGiehl@users.noreply.github.com>
2024-02-18 17:03:46 -0300

    Desafio

--- AppGeral/App.js
+++ AppGeral/App.js
@@ -5,31 +5,32 @@ import Pessoa from './src/Pessoa'
 export default function App() {
 
 const [feed, setFeed] = useState([
-  {_id: '1', nome: 'Luiz', idade: 16, email: 'luiz@gmail.com'},
-  {_id: '2', nome: 'José', idade: 20, email: 'jose@gmail.com'},
-  {_id: '3', nome: 'Carlinhos', idade: 55, email: 'DalvaCavalo@gmail.com'},
-  {_id: '4', nome: 'Antonio', idade: 17, email: 'vesgo@gmail.com'},
-  {_id: '5', nome: 'CalvãoBoeno', idade: 69, email: 'calvo@gmail.com'},
-  {_id: '6', nome: 'Alberto', idade: 79, email: 'alberto@gmail.com'}
-]);
+  {_id: 1, nome: 'Luiz', cargo: 'Estudante',}
+])
 
   return (
-    <View style={styles.container}>
+    <View>
+      <Text style={styles.Text}>Sejá Bem Vindo!!!!</Text>
+
       
       <FlatList
       data={feed}
       keyExtractor={(item) => item._id}
-      renderItem={ ({ item }) => <Pessoa data={item} /> }
-      showsVerticalScrollIndicator={false}
-      />
+      renderItem={ ({item}) => <Pessoa data={item} />}
+      >
+
+      </FlatList>
 
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
+  Text: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 30
+  }
 })
--- AppGeral/src/Pessoa/index.js
+++ AppGeral/src/Pessoa/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-function Pessoa(props){
+function Pessoa(propriedade){
+
     return(
-      <View style={styles.areaPessoa}>
-        <Text style={styles.textPessoa}> {props.data.nome} </Text>
-        <Text style={styles.textPessoa}> {props.data.idade} anos </Text>
-        <Text style={styles.textPessoa}> {props.data.email} </Text>
+      <View  style={styles.pessoa} >
+        <Text style={styles.text} > {propriedade.data.nome} </Text>
+        <Text style={styles.text} > {propriedade.data.cargo} </Text>
       </View>
     );
   }
 
 const styles = StyleSheet.create({
-    areaPessoa: {
-        backgroundColor: '#121212',
-        height: 200,
-        marginBottom: 15,
-        justifyContent: 'center',
-      },
-      textPessoa: {
-        color: '#FFF',
-        fontSize: 20,
-      }
+  pessoa: {
+    fontSize: 30,
+    textAlign: 'center',
+    backgroundColor: '#6699',
+    borderRadius: 6,
+    padding: 15,
+  },
+  text: {
+    fontSize: 20
+  }
 })
-  
+
+
+
 export default Pessoa;
